42a0b77ba4093c4fbf43fcd4a196ad6cc6ba0e95
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-07-30 23:01:24 +0300

    Update methods in block form

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -1,20 +1,62 @@
 const MyForm = {
     submitBtn: document.querySelector('#submitButton'),
-
-    setupListeners: function() {
-        this.submitBtn.addEventListener('click', this.submit);
+    validationRules: {
+        fio: {
+            wordsLength: 3
+        },
+        email: {
+            email: true,
+            domain: ['ya.ru', 'yandex.ru', 'yandex.ua', 'yandex.by', 'yandex.kz', 'yandex.com']
+        },
+        phone: {
+            startWith: '+7',
+            pattern: '+7(999)999-99-99',
+            digitSum: 30
+        }
     },
 
+    /**
+     * Add events listeners
+     */
+    setupListeners: function() {
+        this.submitBtn.addEventListener('click', this.submit.bind(this));
+    },
+
+    /**
+     * Validate form
+     */
     validate: function() {
         //
     },
 
+    /**
+     * Retrieve data from inputs
+     */
+    getData: function() {
+
+    },
+
+    /**
+     * Set data for inputs
+     * @param {Object} data
+     */
+    setData: function(data) {
+
+    },
+
+    /**
+     * Submit form
+     * @param {Object} event
+     */
     submit: function(event) {
         event.preventDefault();
 
-        // console.log('submit');
+        console.log(this);
     },
 
+    /**
+     * Init form object
+     */
     init: function() {
         this.setupListeners();
     }
